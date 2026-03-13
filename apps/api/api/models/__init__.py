2c723e58b6ee98ad97af54c0a6c2539a689435ba
unknown <vladrichicinschi@gmail.com>
2026-03-13 13:10:23 +0100

    Export ActivityType from models

--- apps/api/api/models/__init__.py
+++ apps/api/api/models/__init__.py
@@ -1,6 +1,6 @@
 """Database models."""
 
-from api.models.activity import Activity
+from api.models.activity import Activity, ActivityType
 from api.models.auth_token import AuthToken
 from api.models.bookmark import Bookmark
 from api.models.draft import Draft
@@ -8,4 +8,4 @@ from api.models.progress import Progress
 from api.models.submission import Submission, SubmissionComment
 from api.models.user import User
 
-__all__ = ["User", "Progress", "Draft", "Bookmark", "AuthToken", "Activity", "Submission", "SubmissionComment"]
+__all__ = ["User", "Progress", "Draft", "Bookmark", "AuthToken", "Activity", "ActivityType", "Submission", "SubmissionComment"]
