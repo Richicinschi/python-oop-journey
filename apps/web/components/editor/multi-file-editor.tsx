24db4050dcaebd04678f0c8d9990144145303186
unknown <vladrichicinschi@gmail.com>
2026-03-13 22:43:47 +0100

    fix: add guard for file.id in handleSelectFile (was lost in revert)

--- apps/web/components/editor/multi-file-editor.tsx
+++ apps/web/components/editor/multi-file-editor.tsx
@@ -123,7 +123,7 @@ export function MultiFileEditor({
   }, [dialogState.item, project]);
 
   const handleSelectFile = useCallback((file: ProjectFile) => {
-    project.openFile(file.id);
+    if (file.id) project.openFile(file.id);
   }, [project]);
 
   const handleToggleFolder = useCallback((folder: ProjectFolder) => {
